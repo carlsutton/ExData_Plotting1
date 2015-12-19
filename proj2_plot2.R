d476f1b28e1eb6fb793edabf850c8f3cce1d00f6
Carl Sutton <suttoncarl@ymail.com>
2015-12-19 14:05:15 -0800

    Projecr 2, plot 2 code, Exploratory Data Analysis

---
+++ proj2_plot2.R
@@ -0,0 +1,55 @@
+#  Course project 2
+#  read in data
+nei <- readRDS("summarySCC_PM25.rds")
+scc <- readRDS("Source_Classification_Code.rds")
+
+require(data.table)
+require(dplyr)
+require(ggplot2)
+#  Base R method
+anyNA(nei$Emissions)  #  check for NA's
+anyNA(nei$year)
+#  subset data by year, then sum by year
+nei.emmis.1999 <- subset(nei, year == 1999)
+nei.emmis.1999 <- subset(nei.emmis.1999, fips == "24510")
+head(nei.emmis.1999)
+emiss.1999 <- sum(nei.emmis.1999$Emissions, na.rm = TRUE)
+nei.emmis.2002 <- subset(nei, year == 2002 & fips == "24510")
+head(nei.emmis.2002)
+emiss.2002 <- sum(nei.emmis.2002$Emissions, na.rm = TRUE)
+nei.emmis.2005 <- subset(nei, year == 2005 & fips == "24510")
+head(nei.emmis.2005)
+emiss.2005 <- sum(nei.emmis.2005$Emissions, na.rm = TRUE)
+nei.emmis.2008 <- subset(nei, year == 2008 & fips == "24510")
+head(nei.emmis.2008)
+emiss.2008 <- sum(nei.emmis.2008$Emissions, na.rm = TRUE)
+emiss <- c(emiss.1999,emiss.2002,emiss.2005,emiss.2008)
+emiss
+years <- c(1999,2002,2005,2008)
+plot1 <- plot(years, emiss)
+title(main = "Base Plot of Baltimore City Emissions vs Years")
+ylab = "Emissions"
+dev.copy(png, file = "plot2.png")
+dev.off()
+dev.cur()  #  making sure it got back to screen
+
+plot(years, emiss)
+ylab = "Emissions"
+title(main = "Base Plot of Baltimore City Emissions vs Years")
+
+#  just for yukes lets do this with data table
+dt <- data.table(nei)
+setkey(dt,year)
+dt.emiss.plot <- dt[fips == "24510",.(emissions = sum(Emissions)), by = .(year)]
+ggplot(dt.emiss.plot, aes(year, emissions)) + 
+        geom_point(color = "red", size = 8) +
+        ggtitle("DataTable Version using ggplot")
+dt.emiss.plot
+#  and to show off, how about dplyr
+nei.tbl <- tbl_df(nei)
+summ.nei <- nei.tbl %>% group_by(year) %>%
+        filter(fips = "24510")
+        summarise(emissions = sum(Emissions)) 
+plot(summ.nei$year, summ.nei$emissions)
+title(main = "DPLYR version Emissions vs Years")
+
