cfce2c585896f43861021e73d57ff23990cf2f35
Carl Sutton <suttoncarl@ymail.com>
2015-12-21 18:12:27 -0800

    Plot #2 for Course project 2, Exploratory Data Analysis

--- proj2_plot2.R
+++ proj2_plot2.R
@@ -1,4 +1,9 @@
 #  Course project 2
+#  Have total emissions from PM2.5 decreased in the Baltimore City, Maryland 
+#  (fips == "24510") from 1999 to 2008? Use the base plotting system to make a plot 
+#  answering this question.
+
+
 #  read in data
 nei <- readRDS("summarySCC_PM25.rds")
 scc <- readRDS("Source_Classification_Code.rds")
@@ -26,16 +31,8 @@ emiss.2008 <- sum(nei.emmis.2008$Emissions, na.rm = TRUE)
 emiss <- c(emiss.1999,emiss.2002,emiss.2005,emiss.2008)
 emiss
 years <- c(1999,2002,2005,2008)
-plot1 <- plot(years, emiss)
-title(main = "Base Plot of Baltimore City Emissions vs Years")
-ylab = "Emissions"
-dev.copy(png, file = "plot2.png")
-dev.off()
-dev.cur()  #  making sure it got back to screen
-
-plot(years, emiss)
-ylab = "Emissions"
-title(main = "Base Plot of Baltimore City Emissions vs Years")
+plot(years, emiss,ylab = "Emissions", main = "Base Plot of Baltimore City Emissions
+     vs Years")
 
 #  just for yukes lets do this with data table
 dt <- data.table(nei)
@@ -48,8 +45,12 @@ dt.emiss.plot
 #  and to show off, how about dplyr
 nei.tbl <- tbl_df(nei)
 summ.nei <- nei.tbl %>% group_by(year) %>%
-        filter(fips = "24510")
+        filter(fips == "24510") %>%
         summarise(emissions = sum(Emissions)) 
 plot(summ.nei$year, summ.nei$emissions)
-title(main = "DPLYR version Emissions vs Years")
+title(main = "DPLYR version Baltimore City Emissions vs Years")
 
+plot1 <- plot(years, emiss,ylab = "Emissions", main = "Base Plot of Baltimore City
+             Emissions vs Years")
+dev.copy(png, file = "plot2.png")
+dev.off()
